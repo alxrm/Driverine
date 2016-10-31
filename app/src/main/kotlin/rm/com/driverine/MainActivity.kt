ed52c60838f2489a2be5815d273c3e48835b7f06
alex <rupta3@gmail.com>
2016-11-01 00:35:23 +0300

    Added database connection(!)

--- app/src/main/kotlin/rm/com/driverine/MainActivity.kt
+++ app/src/main/kotlin/rm/com/driverine/MainActivity.kt
@@ -2,8 +2,11 @@ package rm.com.driverine
 
 import android.app.Fragment
 import android.app.FragmentTransaction
+import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.raizlabs.android.dbflow.config.FlowConfig
+import com.raizlabs.android.dbflow.config.FlowManager
 import org.jetbrains.anko.onClick
 import org.jetbrains.anko.setContentView
 import rm.com.driverine.ui.fragment.AdditionFragment
@@ -12,6 +15,11 @@ import rm.com.driverine.ui.view.layout.add
 import rm.com.driverine.ui.view.layout.layout
 
 class MainActivity : AppCompatActivity() {
+
+  override fun attachBaseContext(newBase: Context?) {
+    super.attachBaseContext(newBase)
+    FlowManager.init(FlowConfig.Builder(applicationContext).openDatabasesOnInit(true).build())
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
